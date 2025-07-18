14c0a8acc8293a278b956ff8511cc73904f17df0
Hasan Moosavi <h.mousavi910@gmail.com>
2025-07-18 16:43:15 +0330

    Fix Bug useSearchParam

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from "react";
+
 import LoginForm from "@/components/modules/login-form";
 
 const LoginPage = () => {
-  return <LoginForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
+    </Suspense>
+  );
 };
 
 export default LoginPage;
